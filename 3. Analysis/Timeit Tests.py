bf3286b31a5c759289099cd3e0f7dbb2a9ed4024
nanup <nanup.personal+github@gmail.com>
2022-04-25 19:25:49 +0530

    creates Timeit Tests.py

---
+++ 3. Analysis/Timeit Tests.py
@@ -0,0 +1,13 @@
+import timeit
+import random
+
+print(f"{'n':10s}{'list':>10s}{'dict':>10s}")
+
+for i in range(10_000, 1_000_001, 20_000):
+    t = timeit.Timer(f"random.randrange({i}) in x", 
+    "from __main__ import random, x")
+    x = list(range(i))
+    list_time = t.timeit(number = 1000)
+    x = {j: None for j in range(i)}
+    dict_time = t.timeit(number = 1000)
+    print(f"{i:<10}{list_time:>10.3f}{dict_time:>10.3f}")
