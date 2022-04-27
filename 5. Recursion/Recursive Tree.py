cb0d7daa560ebd611b384da1a25076a0cdd7b212
nanup <nanup.personal+github@gmail.com>
2022-04-27 23:39:57 +0530

    improved recursion

--- 5. Recursion/Recursive Tree.py
+++ 5. Recursion/Recursive Tree.py
@@ -1,24 +1,53 @@
 import turtle
+import random
 
 def tree(branch_len, t):
+    angle = random.randrange(15, 46)
+    num = random.randrange(10, 16)
     if branch_len > 5:
+        t.width(branch_len // 4)
+        if branch_len < 30:
+            t.color("green")
+        else:
+            t.color("brown")
         t.forward(branch_len)
-        t.right(20)
-        tree(branch_len - 15, t)
-        t.left(40)
-        tree(branch_len - 15, t)
-        t.right(20)
+        t.right(angle)
+        tree(branch_len - num, t)
+        t.left(2 * angle)
+        tree(branch_len - num, t)
+        if branch_len < 30:
+            t.color("green")
+        else:
+            t.color("brown")
+        t.right(angle)
         t.backward(branch_len)
+
+        t.width(branch_len // 4)
+        if branch_len < 30:
+            t.color("green")
+        else:
+            t.color("brown")
+        t.forward(branch_len)
+        t.right(angle - 15)
+        tree(branch_len - num, t)
+        t.left(2 * angle - 30)
+        tree(branch_len - num, t)
+        if branch_len < 30:
+            t.color("green")
+        else:
+            t.color("brown")
+        t.right(angle - 15)
+        t.backward(branch_len)
+
 
 def main():
     t = turtle.Turtle()
-    my_win = turtle.Screen()
+    win = turtle.Screen()
     t.left(90)
     t.up()
     t.backward(100)
     t.down()
-    t.color("green")
     tree(75, t)
-    my_win.exitonclick()
+    win.exitonclick()
 
 main()
