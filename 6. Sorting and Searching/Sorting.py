d9c569430db9e0abb4e819cb336ed4e7ebdc2a2d
nanup <85296488+nanup@users.noreply.github.com>
2022-04-30 11:59:38 +0530

    adds code for bubble sort

---
+++ 6. Sorting and Searching/Sorting.py
@@ -0,0 +1,12 @@
+def bubble_sort(self, a_list):
+    for i in range(len(a_list) - 1, 0, -1):
+        exchanges = False
+        for j in range(i):
+            if a_list[j] > a_list[j + 1]:
+                exchanges = True
+                a_list[j], a_list[j + 1] = a_list[j + 1], a_list[j]
+            else:
+                exchanges = False
+        
+        if not exchanges:
+            return a_list
