f6fc206971236621b133c69dd4fa99b143ec6627
nanup <nanup.personal+github@gmail.com>
2022-05-02 12:26:54 +0530

    adds insert sort implementation

--- 6. Sorting and Searching/Sorting.py
+++ 6. Sorting and Searching/Sorting.py
@@ -12,7 +12,7 @@ def bubble_sort(a_list):
             break
 
 def selection_sort(a_list):
-     for i in range(len(a_list) - 1, 0, -1):
+    for i in range(len(a_list) - 1, 0, -1):
         biggest = i
         for j in range(i):
             if a_list[j] > a_list[biggest]:
@@ -21,6 +21,16 @@ def selection_sort(a_list):
         if biggest != i:
             a_list[biggest], a_list[i] = a_list[i], a_list[biggest]
 
+def insert_sort(a_list):
+    for i in range(1, len(a_list)):
+        cur_pos = i
+        cur_val = a_list[i]
+
+        while cur_pos > 0 and a_list[cur_pos - 1] > cur_val:
+            a_list[cur_pos] = a_list[cur_pos - 1]
+            cur_pos -= 1
+        a_list[cur_pos] = cur_val
+
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-selection_sort(a_list)
+insert_sort(a_list)
 print(a_list)
