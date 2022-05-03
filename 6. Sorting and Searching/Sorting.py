09864e6802aac9d61d0061490a684f6bd635ba93
nanup <nanup.personal+github@gmail.com>
2022-05-03 19:38:48 +0530

    adds quick sort implementation

--- 6. Sorting and Searching/Sorting.py
+++ 6. Sorting and Searching/Sorting.py
@@ -79,7 +79,50 @@ def merge_sort(a_list):
             k += 1
         print("Merging", a_list)
 
+def quick_sort(a_list):
+    quick_sort_helper(a_list, 0, len(a_list) - 1)
+
+def quick_sort_helper(a_list, first, last):
+    if first < last:
+        split = partition(a_list, first, last)
+        partition(a_list, first, split - 1)
+        partition(a_list, split + 1, last)
+
+def partition(a_list, first, last):
+    mid = a_list[len(a_list[first:last + 1]) // 2]
+    pivot_list = [a_list[first], mid, a_list[last]]
+    pivot_list.sort()
+    pivot = pivot_list[len(pivot_list) // 2]
+    pivot_pos = a_list.index(pivot)
+
+    if pivot_pos == first:
+        left = first + 1
+        right = last
+    elif pivot_pos == last:
+        left = first
+        right = last - 1
+    else:
+        left = first
+        right = last
+
+    done = False
+
+    while not done:
+        while a_list[left] <= a_list[right] and a_list[right] >= pivot:
+            right -= 1
+        while a_list[left] <= a_list[right] and a_list[left] <= pivot:
+            left += 1
+        if right < left:
+            done = True
+        else:
+            a_list[left], a_list[right] = a_list[right], a_list[left]
+    if pivot >= a_list[right] and pivot_pos <= right:
+        a_list[pivot_pos], a_list[right] = a_list[right], a_list[pivot_pos]
+    else:
+        a_list[pivot_pos], a_list[right + 1] = a_list[right + 1], a_list[pivot_pos]
+
+    return right
 
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-merge_sort(a_list)
+quick_sort(a_list)
 print(a_list)
