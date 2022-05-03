1d040e74a0c3d8c3be09abbc74c35f4714136da4
nanup <nanup.personal+github@gmail.com>
2022-05-03 17:51:16 +0530

    adds merge sort implementation

--- 6. Sorting and Searching/Sorting.py
+++ 6. Sorting and Searching/Sorting.py
@@ -48,6 +48,38 @@ def insertion_gap_sort(a_list, start, gap):
             cur_pos -= gap
         a_list[cur_pos] = cur_val
 
+def merge_sort(a_list):
+    if len(a_list) > 1:
+        print("Splitting", a_list)
+        mid = len(a_list) // 2
+        left_half = a_list[:mid]
+        right_half = a_list[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i, j, k = 0, 0, 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] <= right_half[j]:
+                a_list[k] = left_half[i]
+                i += 1
+            else:
+                a_list[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            a_list[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            a_list[k] = right_half[j]
+            j += 1
+            k += 1
+        print("Merging", a_list)
+
+
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-shell_sort(a_list)
+merge_sort(a_list)
 print(a_list)
