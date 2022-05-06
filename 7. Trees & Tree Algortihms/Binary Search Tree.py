23c5982316e6b92dfec9ed5742c323960d65016f
nanup <nanup.personal+github@gmail.com>
2022-05-06 11:48:29 +0530

    creates Binary Search Tree.py

---
+++ 7. Trees & Tree Algortihms/Binary Search Tree.py
@@ -0,0 +1,49 @@
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def size(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+class TreeNode:
+    def __init__(self, key, value, parent = None, left_child = None, right_child = None):
+        self.key = key
+        self.value = value
+        self.parent = parent
+        self.left_child = left_child
+        self.right_child = right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child is self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child is self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.left_child or self.right_child)
+
+    def has_a_child(self):
+        return self.right_child or self.left_child
+
+    def has_children(self):
+        return self.right_child and self.left_child
+
+    def replace_value(self, key, value, left, right):
+        self.key = key
+        self.value = value
+        self.left_child = left
+        self.right_child = right
+        if self.left_child:
+            self.left_child.parent = self
+        if self.right_child:
+            self.right_child.parent = self
