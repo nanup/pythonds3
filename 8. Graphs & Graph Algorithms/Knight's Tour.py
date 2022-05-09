e90dacaf04d05f032e06711f9623eb6bc4223506
nanup <nanup.personal+github@gmail.com>
2022-05-09 19:09:52 +0530

    creates Knight's Tour.py

---
+++ 8. Graphs & Graph Algorithms/Knight's Tour.py
@@ -0,0 +1,52 @@
+from pythonds3.graphs import Graph
+
+def knightGraph(bdSize):
+    ktGraph = Graph()
+    for row in range(bdSize):
+        for col in range(bdSize):
+            nodeId = posToNodeId(row, col, bdSize)
+            newPositions = genLegalMoves(row, col, bdSize)
+            for e in newPositions:
+                nId = posToNodeId(e[0], e[1], bdSize)
+                ktGraph.addEdge(nodeId, nId)
+    return ktGraph
+
+def posToNodeId(row, col, board_size):
+    return (row * board_size) + col
+
+def genLegalMoves(x, y, bdSize):
+    newMoves = []
+    moveOffsets = [(-1, -2), (-1, 2), (-2, -1), (-2, 1),
+                   ( 1, -2), ( 1, 2), ( 2, -1), ( 2, 1)]
+    for i in moveOffsets:
+        newX = x + i[0]
+        newY = y + i[1]
+        if legalCoord(newX, bdSize) and legalCoord(newY, bdSize):
+            newMoves.append((newX, newY))
+    return newMoves
+
+def legalCoord(x, bdSize):
+    if x >= 0 and x < bdSize:
+        return True
+    else:
+        return False
+
+def knightTour(current_depth, vertex_list, current_vertex, node_limit):
+    current_vertex.set_color('gray')
+    vertex_list.append(current_vertex)
+    if current_depth < node_limit:
+        nbrList = list(current_vertex.getConnections())
+        i = 0
+        done = False
+        while i < len(nbrList) and not done:
+            if nbrList[i].getColor() == 'white':
+                done = knightTour(current_depth + 1, vertex_list, nbrList[i], node_limit)
+                I += 1
+        if not done:
+            vertex_list.pop()
+            current_vertex.setColor('white')
+
+    else:
+        done = True
+
+    return done
